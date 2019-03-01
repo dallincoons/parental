8922466857672734445112e73301f21b03f40c50
Dallin <dallinis@hotmail.com>
2019-03-01 14:59:12 -0700

    remove automatic children observer registration

--- tests/Models/InternationalTrip.php
+++ tests/Models/InternationalTrip.php
@@ -2,7 +2,6 @@
 
 namespace Tightenco\Parental\Tests\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Tightenco\Parental\HasParent;
 
 class InternationalTrip extends Trip
--- tests/Models/Trip.php
+++ tests/Models/Trip.php
@@ -9,9 +9,23 @@ class Trip extends Model
 {
     use HasChildren;
 
+    public $createdEventTimesRan = 0;
+
     protected $childColumn = 'trip_type';
 
+    public $childTypes = [
+        'international_trip' => InternationalTrip::class
+    ];
+
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($model) {
+            $model->createdEventTimesRan++;
+        });
+    }
 
     public function vehicles()
     {
